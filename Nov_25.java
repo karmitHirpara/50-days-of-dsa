005c07a7b23de805a8638773ef5a21a53b533d4c
KarmitHirpara <156499383+karmitHirpara@users.noreply.github.com>
2025-11-25 10:53:30 +0530

    leecode 1015

---
+++ Nov_25.java
@@ -0,0 +1,20 @@
+//1015:) Smallet integer divisible by K
+
+// --------------------------------------------------------------------
+// Given a positive integer k, you need to find the smallest positive integer n such that
+// n is divisible by k, and n only contains the digit 1.
+// Return the length of n. If there is no such n, return -1.
+// --------------------------------------------------------------------
+
+class Solution {
+    public int smallestRepunitDivByK(int k) {
+        if(k%2==0 || k%5==0) return -1;
+        
+        int rem = 0;
+        for(int i = 0; i<k; i++){
+            rem = ((rem*10)+1)%k;
+            if(rem%k==0) return ++i;
+        }
+        return -1;
+    }
+}
