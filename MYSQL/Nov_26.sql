a047567305765f4e27c1a197e40ef761132772be
KarmitHirpara <156499383+karmitHirpara@users.noreply.github.com>
2025-11-26 22:39:27 +0530

    Add files via upload
    Add new MySQL LeetCode solutions (Employee Bonus & Followers Count)

--- MYSQL/Nov_26.sql
+++ MYSQL/Nov_26.sql
@@ -1,5 +1,7 @@
--- LeetCode 577: Employee Bonus
--- Employee with bonus less than 1000 or no bonus (LeetCode style)
+/* ============================================================
+   LeetCode 577: Employee Bonus
+   Return employees whose bonus is less than 1000 or NULL
+   ============================================================ */
 
 SELECT e.name AS name,
        b.bonus
@@ -8,3 +10,17 @@ LEFT OUTER JOIN Bonus b
        ON b.empId = e.empId
 WHERE b.bonus < 1000
    OR b.bonus IS NULL;
+
+
+
+/* ============================================================
+   LeetCode 1729: Find Followers Count
+   For each user, return the number of followers
+   Results must be ordered by user_id ascending
+   ============================================================ */
+
+SELECT user_id,
+       COUNT(follower_id) AS followers_count
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id ASC;
