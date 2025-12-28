ca43112bb3711f519421a957abce3a17edb5033b
KarmitHirpara <156499383+karmitHirpara@users.noreply.github.com>
2025-12-28 18:57:29 +0530

    Add two approaches for counting negatives in matrix
    Replaces the original countNegatives method with countNegatives_approach_1, which sorts each row and uses binary search to count negatives. Also introduces countNegatives_approach_2, which uses a nested loop to count negatives. Updates main to use the first approach.

--- DSA/Dec_28.java
+++ DSA/Dec_28.java
@@ -6,11 +6,32 @@ package DSA;
 public class Dec_28 {
     public static void main(String[] args) {
         int[][] a = {{4, 3, 2, -1}, {3, 2, 1, -1}, {1, 1, -1, -2}, {-1, -1, -2, -3}};
-        int result = countNegatives(a);
+        int result = countNegatives_approach_1(a);
         System.out.println(result);
     }
     
-    public static int countNegatives(int[][] a) {
+
+    private int countNegatives_approach_1(int[][] a) {
+        int c = 0 ;
+        for(int []x : a){
+            Arrays.sort(x);
+        }
+        for(int []x : a){
+            int l = 0, r = x.length-1;
+            while (l<=r){
+                int mid = (l+r)/2;
+                if(x[mid]<0){
+                    l = mid+1;
+                }else{
+                    r = mid-1;
+                }
+            }
+            c+=l;
+        }
+        return c;
+    }
+
+    public static int countNegatives_approach_2(int[][] a) {
         int c = 0 ;
         for(int i = 0 ; i<a.length;i++){
             for(int j = 0;j<a[0].length;j++){
