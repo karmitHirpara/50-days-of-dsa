ac8bb98510b5d9196e26c97830d7648ff14d7f56
KarmitHirpara <csbsccs079@gmail.com>
2026-04-22 18:21:49 +0530

    Make helper methods static and add demo
    Add a LeetCode problem comment and a simple main demo that calls minMirrorPairDistance. Convert minMirrorPairDistance, mirrorDistance, abs, and rev to static so they can be invoked from main. Minor formatting adjustments and sample input/output print added for quick local testing.

--- DSA/Apr_18.java
+++ DSA/Apr_18.java
@@ -1,14 +1,18 @@
 package DSA;
 
-import java.util.HashMap;
+// Leetcode 3761. Minimum Absolute Distance Between Mirror Pairs
+// ``````````````````````````````````````````````````````````````
 
+import java.util.HashMap;
 public class Apr_18 {
 
     public static void main(String[] args) {
-        
+        int a[] = {1, 23, 456, 321, 54};
+        int ans = minMirrorPairDistance(a);
+        System.out.println(ans);
     }
 
-    public int minMirrorPairDistance(int[] a) {
+    public static int minMirrorPairDistance(int[] a) {
         HashMap<Integer, Integer> map = new HashMap<>();
         int min = Integer.MAX_VALUE;
 
@@ -24,16 +28,16 @@ public class Apr_18 {
         return min == Integer.MAX_VALUE ? -1 : min;
     }
 
-    private int mirrorDistance(int n) {
+    private static int mirrorDistance(int n) {
         int rev = rev(n);
         return abs(rev - n);
     }
 
-    private int abs(int n){
+    private static int abs(int n){
         return (n>0) ? n : n*-1;  
     }
 
-    private int rev(int n){
+    private static int rev(int n){
         StringBuilder sb = new StringBuilder(n+"");
         return Integer.parseInt(sb.reverse().toString());
     }
